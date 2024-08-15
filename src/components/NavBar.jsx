050c4dec6231ee5a271b68d7f560f16d9422832f
hannington owiti <hanningtonowiti@gmail.com>
2024-08-15 21:22:25 +0300

    NavBar-styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = ({cartCount}) => (
-    <header className="header">
-        <h1>Courses</h1>
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/courses">Courses</Link>
-            <Link to="/cart">cart ({cartCount})</Link>
+    <header className="bg-lime-900 text-white py-10 px-8 flex justify-between items-center mb-8">
+        <h1 className="text-2xl font-bold ">Courses</h1>
+        <nav className="space-x-4">
+            <Link to="/" className= "px-6 py-3 hover:text-amber-500 transition-colors duration-300">Home</Link>
+            <Link to="/courses" className="px-6 py-3 hover:text-amber-500 transition-colors duration-300">Courses</Link>
+            <Link
+            to="/cart"
+            className="px-6 py-3 hover:text-amber-500 transition-colors duration-300"
+            >
+                cart <i className="fas fa-shopping-cart text-xl"></i>
+                {cartCount > 0 && (
+                <span className='ml-2 text-lg'>{cartCount}</span>
+            )}
+            </Link>
         </nav>
     </header>
 );
